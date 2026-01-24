e0e47b3519455a1134b156b90c226c1cd8eb26fd
sanjai3102001 <sanjai99522s@gmail.com>
2026-01-24 19:57:52 +0530

    adding restart command

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the server
-systemctl start httpd
+systemctl restart httpd
 systemctl enable httpd
 # Ensure permissions are correct for my location
 chown -R apache:apache /var/www/html
