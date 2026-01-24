b31a71906fcbc64436b8fd1d1df880ec82d98a69
sanjai3102001 <sanjai99522s@gmail.com>
2026-01-24 17:46:22 +0530

    adding chown insted of apache

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 systemctl start httpd
 systemctl enable httpd
 # Ensure permissions are correct
-chown -R ec2-user:ec2-user /home/ec2-user/my-app
+apache:apache -R ec2-user:ec2-user /home/ec2-user/my-app
