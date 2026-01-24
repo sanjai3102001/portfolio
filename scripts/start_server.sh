b3fdc7d292194b891c49ca4ccce4d7bbefb3c38f
sanjai3102001 <sanjai99522s@gmail.com>
2026-01-24 18:01:54 +0530

    adding chown insted of apache

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 # Start the server
 systemctl start httpd
 systemctl enable httpd
-# Ensure permissions are correct
+# Ensure permissions are correct for my location
 chown -R apache:apache /var/www/html
