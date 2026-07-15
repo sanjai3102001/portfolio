50dcb8f81b69581671c011c08ce285200cb591d7
sanjai3102001 <sanjai99522s@gmail.com>
2026-07-16 01:16:49 +0530

    adding estart command and changeing from start to restart

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Example: Restart Nginx to pick up new static files or config
+
+# Change ownership of the web root to the Apache user
+chown -R apache:apache /var/www/html
+
+# Restart the Apache web server to apply changes
 systemctl restart httpd
