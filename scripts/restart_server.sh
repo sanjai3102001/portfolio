dfc61841a1bf646bfcfdac964140e919ccf87771
sanjai3102001 <sanjai99522s@gmail.com>
2026-07-16 01:22:52 +0530

    adding selinux command

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -3,5 +3,8 @@
 # Change ownership of the web root to the Apache user
 chown -R apache:apache /var/www/html
 
+# Restore the correct SELinux context for web content
+restorecon -R /var/www/html
+
 # Restart the Apache web server to apply changes
 systemctl restart httpd
